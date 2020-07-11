bd154ec0100a421471de34abe03fbb38ab8bc465
Natalya-Zay <mersedes_89@mail.ru>
2020-07-11 09:06:08 +0300

    My first commit

---
+++ src/introduced.java
@@ -0,0 +1,15 @@
+public class introduced {
+
+    public static void main(String[] args) {
+
+       for (int i=0; i<5; i++){
+           System.out.print("My name is Natasha. " + " ");
+       }
+        System.out.println();
+
+        for (int i=0; i<5; i++){
+            System.out.println("I want to be a java daveloper!!!!! " + " ");
+        }
+    }
+}
+
