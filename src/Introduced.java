31569c222327c16b07088a9f1c0374ae03701939
Natalya-Zay <mersedes_89@mail.ru>
2020-07-13 16:09:58 +0300

    Rename class, remove space

--- src/Introduced.java
+++ src/Introduced.java
@@ -1,14 +1,11 @@
-public class introduced {
-
+public class Introduced {
     public static void main(String[] args) {
-
        for (int i=0; i<5; i++){
            System.out.print("My name is Natasha. " + " ");
        }
         System.out.println();
-
         for (int i=0; i<5; i++){
-            System.out.println("I want to be a java daveloper!!! " + " ");
+            System.out.println("I want to be a java developer!!! " + " ");
         }
     }
 }
