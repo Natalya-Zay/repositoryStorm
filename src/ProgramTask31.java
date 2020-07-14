0f664de9cf62e31bf684eafb71cc20200dc6a8bc
Natalya-Zay <mersedes_89@mail.ru>
2020-07-14 17:50:01 +0300

    Homework3

---
+++ src/ProgramTask31.java
@@ -0,0 +1,9 @@
+public class ProgramTask31 {
+    public static void main(String[] args) {
+        byte a = 127; // 1 байт ; значений (-128;127);
+        int b = 255;
+       // int c1= a-b;
+         byte c2 = 127-255;
+        System.out.println(c2);
+    }
+}
---
+++ src/ProgramTask32.java
@@ -0,0 +1,8 @@
+public class ProgramTask32 {
+    public static void main(String[] args) {
+        short a = -32768; // 2 байта ; значений (-32768;32767);
+        int b = 32768;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
