23aa3b2142e284452e7164ec29ce17a39f33db0a
Natalya-Zay <mersedes_89@mail.ru>
2020-07-17 13:18:22 +0300

    Homework4

---
+++ src/com/github/NatalyaZay/ProgramTask44.java
@@ -0,0 +1,32 @@
+package com.github.NatalyaZay;
+
+public class ProgramTask44 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        boolean x = true;          // a != b
+        System.out.println(x);
+        System.out.println();
+
+        int a1 = 5;
+        int b1 = 5;
+        System.out.println(a1>b1);// boolean .
+        System.out.println(a1<b1);
+        System.out.println(a1>=b1);
+        System.out.println(a1<=b1);
+        System.out.println(a1==b1);
+        System.out.println(a1!=b1);
+
+        boolean c = false;       // a != b
+        System.out.println(c);
+
+
+
+    }
+}
