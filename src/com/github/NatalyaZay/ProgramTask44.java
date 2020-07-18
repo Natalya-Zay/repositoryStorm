c8b36839f2bc1d465eecc75f7984f581a2bf38ad
Natalya-Zay <mersedes_89@mail.ru>
2020-07-18 08:39:40 +0300

    Homework4.added comments

--- src/com/github/NatalyaZay/ProgramTask44.java
+++ src/com/github/NatalyaZay/ProgramTask44.java
@@ -10,19 +10,20 @@ public class ProgramTask44 {
         System.out.println(a<=b);
         System.out.println(a==b);
         System.out.println(a!=b);
+        System.out.println();
         boolean x = true;          // a != b
         System.out.println(x);
         System.out.println();
 
         int a1 = 5;
         int b1 = 5;
-        System.out.println(a1>b1);// boolean .
-        System.out.println(a1<b1);
-        System.out.println(a1>=b1);
-        System.out.println(a1<=b1);
-        System.out.println(a1==b1);
-        System.out.println(a1!=b1);
-
+        System.out.println(a1>b1);// boolean . 5 не больше 5 false . так как 5 равно 5
+        System.out.println(a1<b1); //  5 не больше 5 false . так как 5 равно 5
+        System.out.println(a1>=b1); // 5 равно 5 , true
+        System.out.println(a1<=b1); // 5 равно 5 , true
+        System.out.println(a1==b1); // 5 равно 5 , true
+        System.out.println(a1!=b1); // в выражении сказано , что 5 НЕ равно 5, это false
+        System.out.println();
         boolean c = false;       // a != b
         System.out.println(c);
 
