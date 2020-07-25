4c8418ffe5225d8dde74a26224fe7d50eeec06e8
Natalya-Zay <mersedes_89@mail.ru>
2020-07-25 12:36:02 +0300

    Homework 6.

---
+++ src/com/github/NatalyaZay/ProgramTask61.java
@@ -0,0 +1,29 @@
+package com.github.NatalyaZay;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ProgramTask61 {
+    public static void main(String[] args) {
+        // 1 задание
+        int [] array = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(array));
+
+        // 2 задание
+        int [] array1 = {1,2,3,4,5,6,7,8,9,10};
+        int [] reverse = new int [array1.length];
+        System.arraycopy(array1,0,reverse,0,10);
+        System.out.println(Arrays.toString(reverse));
+
+        for (int i = 0; i < reverse.length; i++){
+            for (int j =0; j < reverse.length-1; j++){
+               if (reverse[j] < reverse[j+1]) {
+                   int k = reverse[j];
+                   reverse[j] = reverse[j+1];
+                   reverse[j+1] = k;
+               }
+            }
+       }
+        System.out.println(Arrays.toString(reverse));
+    }
+}
---
+++ src/com/github/NatalyaZay/ProgramTask63.java
@@ -0,0 +1,25 @@
+package com.github.NatalyaZay;
+
+import java.util.Arrays;
+
+public class ProgramTask63 {
+    public static void main(String[] args) {
+        // 3 задание
+        int [] array = {1, 2, 3, 4, 5};
+        swap(array,0,1);
+
+        int [] array1 = new int[] {2, 3, 4, 5, 6, 10};
+        swap(array1,2,5);
+    }
+
+    private static void swap(int[] array, int indexA, int indexB) {
+        System.out.println("Trying swap: "+ Arrays.toString(array) + " indexA " + indexA + " and " + indexB );
+
+        for (int i = 0; i < 1; i++){
+             int arr = array[indexA];
+             array[indexA] = array[indexB];
+             array[indexB] = arr;
+     }
+        System.out.println(" Result: " + Arrays.toString(array) + "\n");
+    }
+}
