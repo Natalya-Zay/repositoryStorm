dbf9359314c67d87aa7d7cf1a5ec79ce48024204
Natalya-Zay <mersedes_89@mail.ru>
2020-07-29 22:49:14 +0300

    Homework 7 . add task7

---
+++ src/com/github/NatalyaZay/ProgramTask77.java
@@ -0,0 +1,15 @@
+package com.github.NatalyaZay;
+
+public class ProgramTask77 {
+    public static void main(String[] args) {
+      int people = 10000000;
+      int s = 0;
+      int born = 14;
+      int death = 8;
+      int difference = born-death;
+      for (int i = 1; i <= 10; i++){
+         s = ( people / 1000) * difference * i;
+      }
+        System.out.println(s);
+    }
+}
