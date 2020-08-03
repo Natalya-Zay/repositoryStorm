9fd1bc0328b4368b64ab1865e7d26df1e339172c
Natalya-Zay <mersedes_89@mail.ru>
2020-08-03 15:35:35 +0300

    Homework 7.correct code

--- src/com/github/NatalyaZay/ProgramTask74.java
+++ src/com/github/NatalyaZay/ProgramTask74.java
@@ -3,8 +3,9 @@ package com.github.NatalyaZay;
 public class ProgramTask74 {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7-i ; j++) {
+        int lenth = 10;
+        for (int i = 0; i < lenth/2; i++) {
+            for (int j = 0; j < lenth/2-i ; j++) {
                 System.out.print(" ");
             }
             for (int s = 0; s < i * 2 ; s++){
