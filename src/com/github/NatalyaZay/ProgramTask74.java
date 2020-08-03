2f264591e9da4708254a6e6d16cf96a228f8a5bc
Natalya-Zay <mersedes_89@mail.ru>
2020-08-03 15:41:20 +0300

    Homework 7..

--- src/com/github/NatalyaZay/ProgramTask74.java
+++ src/com/github/NatalyaZay/ProgramTask74.java
@@ -2,6 +2,7 @@ package com.github.NatalyaZay;
 
 public class ProgramTask74 {
     public static void main(String[] args) {
+
         int length = 10 ;
         for (int i = 0; i < length/2; i++) {
             for (int j = 0; j < length/2-i ; j++) {
