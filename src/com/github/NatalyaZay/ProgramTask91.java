7fdb08527e0d2fa5989a87343766842225a03bfc
Natalya-Zay <mersedes_89@mail.ru>
2020-08-04 11:08:07 +0300

    Homework 9

---
+++ src/com/github/NatalyaZay/ProgramTask91.java
@@ -0,0 +1,15 @@
+package com.github.NatalyaZay;
+
+public class ProgramTask91 {
+    public static void main(String[] args) {
+        /*
+           * пул.
+           * объект
+           * пул
+           * пул
+           * ---
+           * объект
+           Итого : 3 объекта в пуле.
+         */
+    }
+}
---
+++ src/com/github/NatalyaZay/ProgramTask93.java
@@ -0,0 +1,11 @@
+package com.github.NatalyaZay;
+
+public class ProgramTask93 {
+    public static void main(String[] args) {
+
+        String st = "ази";
+        String st2 = "мут";
+        System.out.println(st+st2);
+        System.out.println(st.concat(st2));
+    }
+}
