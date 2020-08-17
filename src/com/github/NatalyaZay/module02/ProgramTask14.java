9445d7d2cf95c07f52645ffaed660b82de5e1749
Natalya-Zay <mersedes_89@mail.ru>
2020-08-17 15:45:24 +0300

    Editing the code 1.4

--- src/com/github/NatalyaZay/module02/ProgramTask14.java
+++ src/com/github/NatalyaZay/module02/ProgramTask14.java
@@ -5,20 +5,15 @@ import java.util.Comparator;
 
 public class ProgramTask14 {
     public static void main(String[] args) {
-        Person person = new Person("Vova", 21);
-        Person person1 = new Person("Dima", 26);
-        Person person2 = new Person("Oleg", 18);
-        Person person3 = new Person("Sahsa", 29);
-        Person person4 = new Person("Serega", 27);
-        Person person5 = new Person("Petr", 80);
 
         Person[] people = new Person[6];
-        people[0] = person;
-        people[1] = person1;
-        people[2] = person2;
-        people[3] = person3;
-        people[4] = person4;
-        people[5] = person5;
+        people[0] = new Person("Vova", 21);
+        people[1] = new Person("Dima", 26);
+        people[2] = new Person("Oleg", 18);
+        people[3] = new Person("Sahsa", 29);
+        people[4] = new Person("Serega", 27);
+        people[5] = new Person("Petr", 80);
+
         System.out.println("До сортировки : ");
         System.out.println(Arrays.toString(people));
         System.out.println();
